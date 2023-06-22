690e077539af7b36d4e6ef39aa6d68d1853545e9
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-06-22 09:57:51 +0700

    create api

---
+++ T2204M_API/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using T2204M_API.Entities;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace T2204M_API.Controllers
+{
+    [ApiController]
+    [Route("api/category")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly T2204mApiContext _context;
+
+        public CategoryController(T2204mApiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return Ok(categories);
+        }
+    }
+}
+
---
+++ T2204M_API/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Connection Database
+var connectionString = builder.Configuration.GetConnectionString("T2204M_API");
+builder.Services.AddDbContext<T2204M_API.Entities.T2204mApiContext>(
+        options => options.UseSqlServer(connectionString)
+    );
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
