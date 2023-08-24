374e954c515a59c2f8b22013846b9ade9df27a98
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-08-24 09:30:10 +0700

    demo rabbitmq

---
+++ T2204M_API/Controllers/MessageController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace T2204M_API.Controllers
+{
+    [ApiController]
+    [Route("api/message")]
+    public class MessageController : ControllerBase
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            try
+            {
+                var factory = new ConnectionFactory
+                {
+                    HostName = "localhost",
+                    Port = 5672,
+                    VirtualHost = "/"
+                };
+                var connection = factory.CreateConnection();
+                var channel = connection.CreateModel();
+                channel.QueueDeclare(
+                    queue: "t2204m",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                    );
+
+                const string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(
+                        exchange: string.Empty,
+                        routingKey:"t2204m",
+                        basicProperties:null,
+                        body:body
+                        );
+
+                return Ok();
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
+
